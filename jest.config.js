a0da5ecadd1898a345c30529a43a12e2162e996d
Haroon Aziz <ha01k5295@gmail.com>
2025-11-30 18:27:06 +0500

    ci-cd pipeline cypress token added

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js'],
-  collectCoverage: false, // Disable coverage for now to avoid babel issues
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/cypress/**',
+    '!**/.next/**',
+    '!**/.strapi/**',
+    '!**/jest.config.js',
+    '!**/coverage/**',
+    '!**/cypress.config.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
   testPathIgnorePatterns: [
     '/node_modules/',
     '/.next/',
